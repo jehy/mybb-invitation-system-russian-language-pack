65e741d4bf22c1263846c7770132b9b1acfe6489
Jehy <fate@jehy.ru>
2014-05-30 10:24:14 +0400

    first version

---
+++ invite.lang.php
@@ -0,0 +1,101 @@
+<?php
+/* Copyright (c) 2012 by Christian Fillion.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>. */
+
+$l['reg_invite'] = "Инвайт:";
+$l['reg_invite_desc'] = "Если у вас есть инвайт код, введите его сюда:";
+$l['reg_invite_desc_required'] = "Введите ваш инвайт код сюда:";
+$l['reg_invalid_code'] = "Инвайт код неверный.";
+$l['reg_invalid_code2'] = " Это поле не требуется для регистрации.";
+$l['reg_invite_required'] = "Вы должны ввести инвайт код для регистрации на этом форуме.";
+$l['profile_invite'] = "Инвайт Код";
+$l['profile_notactivated'] = "(В ожидании активации)";
+$l['invites'] = 'Инвайты';
+$l['manage_invites'] = "Управление инвайтами";
+$l['add_invite'] = "Добавить инвайт";
+$l['edit_invite'] = "Редактировать инвайт";
+$l['delete_invite'] = "Удалить";
+$l['adding'] = "Добавление";
+$l['editing'] = "Редактирование";
+$l['invites_emptylist'] = "Здесь нет инвайта для отображения.";
+$l['invite_code'] = "Код";
+$l['invite_used'] = "Использовано";
+$l['invite_maxuses'] = "Лимит";
+$l['invite_members'] = "Использовано/email";
+$l['invite_primarygroup'] = "Главная группа";
+$l['invite_othergroups'] = "Остальные группы";
+$l['invite_othergroups_edit'] = "Остальные группы (необязательно)";
+$l['invite_expire'] = "Истекает";
+$l['invite_expire_edit'] = "Истекает (необязательно, по английски)";
+$l['invite_sendnomail'] = "Не посылать емейл";
+$l['invite_createdby'] = 'Инвайт создан участником:';
+$l['confirm_action'] = "Вы уверены?";
+$l['menu'] = "Меню";
+$l['available_invites'] = "Инвайтов доступно";
+$l['unavailable_invites'] = "Инвайты недоступны";
+$l['email_edit'] = "Резервирование на емейл (необязательно)";
+$l['add_and_close'] = "Добавить инвайт";
+$l['add_and_return'] = "Добавить инвайт и продолжить";
+$l['selected_isnull'] = "Выбранный инвайт не существует.";
+$l['delete_this_invite'] = "Удалить этот инвайт";
+$l['edit_and_close'] = "Обновить Инвайт";
+$l['invalid_form'] = "Некорректная форма. Пожалуйста, попробуйте заново.";
+$l['code_already_used'] = "Этот инвайт код уже использован. Пожалуйста, попробуйте заново.";
+$l['fill_all_fields'] = "Вы должны заполнить все требуемые поля.";
+$l['invalid_email'] = "Этот емейл адрес некорректен. Пожалуйста, попробуйте заново.";
+$l['code_too_short'] = "Инвайд код слишком короткий. Пожалуйста, попробуйте заново.";
+$l['code_too_long'] = "Инвайд код слишком длинный. Пожалуйста, попробуйте заново.";
+$l['invalid_code'] = "Инвайт код содержит некорретные символы.";
+$l['invite_created'] = "Инвайт успешно создан.";
+$l['invite_deleted'] = "Инвайт удалён из базы.";
+$l['invite_edited'] = "Инвайт успешно отредактирован.";
+$l['invite_mailed'] = "Инвайт был выслан на этот адрес:";
+$l['invite_mail_failed'] = "Не получилось вылать инвайт на этот адрес:";
+$l['invite_mail_1'] = "{username} пригласил вас на";
+$l['invite_mail_1_noref'] = "Вы были приглашены на";
+$l['invite_mail_2'] = "Чтобы испоьзовать инвайт, нажмите на ссылку или скопируйте её в ваш браузер.";
+$l['invite_mail_3'] = "Если вы не запрашивали инвайт, просто игнорируйте это письмо.";
+$l['invite_mail_4'] = "Надеемся увидеть вас на нашем сайте,\nкоманда";
+$l['invite_mail_5'] = "Это сообщение было выслано автоматически. Чтобы связаться с нами:";
+$l['invite_mail_group'] = "Група:";
+$l['invite_mail_expire'] = "Истечение:";
+$l['invite_mail_email'] = "Емейл:";
+$l['search_invite'] = "Поиск";
+$l['searchresult_invite'] = "Результаты поиска";
+$l['invite_maxcodes'] = "Вы достигли предельного количества ваших инвайтов.";
+$l['invite_refer_link'] = "Чтобы пригласить на этот форум учасника, направьте его на";
+$l['invite_referredcount'] = "Приглашенные участники:";
+$l['invite_forbiddengroup'] = "Вы не можете пригласить пользователя из этой группы.";
+$l['invites_myinvites'] = "Мои инвайты";
+$l['invite_mail_refme'] = "\"Вас пригласил(а) ...\"";
+$l['invited_yourself'] = "Вы не можете пригласить сам себя!";
+$l['sort'] = "Сортировать по:";
+$l['sort_id'] = "Номер";
+$l['sort_creator'] = "Создатель";
+$l['sort_email'] = "Емейл";
+$l['sort_group'] = "Група";
+$l['sort_used'] = "Использовано";
+$l['sort_maxuses'] = "Лимит";
+$l['sort_expiration'] = "Истечение";
+$l['asc'] = "[прямо]";
+$l['desc'] = "[обратно]";
+$l['invite_multidelete'] = "Массовое удаление";
+$l['multidelete_button'] = "Удалить выбранное";
+$l['delete_invites'] = "Удалить инвайты";
+$l['confirm_multidelete'] = "%d инвайтов будет удалено безвозвратно.";
+$l['no_invite_selected'] = "Инвайты не выбраны.";
+$l['multidelete_deleted'] = "Выбраные инвайты были удалены.";
+$l['cancel_multidelete'] = "Отменить массовое удаление";
+$l['invited_by'] = "Приглашён участником:";
